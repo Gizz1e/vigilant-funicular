7569122eafa278031b00db52b0b2c671f011609f
Gizz1e <gizzletv_llc@proton.me>
2026-03-27 20:17:00 -0500

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import Home from './pages/Home';
+import Browse from './pages/Browse';
+import Live from './pages/Live';
+import Library from './pages/Library';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
+export default function App() {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/browse" element={<Browse />} />
+            <Route path="/live" element={<Live />} />
+            <Route path="/library" element={<Library />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
